eb9f0d7fa2a5876d3d96807a1e5d769183bda6a6
mohammedal-ess <99287125+mohammedal-ess@users.noreply.github.com>
2023-01-19 09:36:28 -0500

    Update game.ts

--- src/game.ts
+++ src/game.ts
@@ -1,27 +1,47 @@
-/*
- * This is Rock Paper Scissors Program
- *
- * @author  Mohammed-Al-Ewssai
- * @version 1.0
- * @since   2022-09-20
+/**
+ * This is a hourglass program.
+ * By:      Mohammed-el-awassi
+ * Version: 1.0
+ * Since:   2022-10-17
  */
 
-import promptSync from "prompt-sync";
-const prompt = promptSync();
+import promptSync from 'prompt-sync'
 
-const choices = ["rock", "paper", "scissors"];
-const randomChoice = choices[Math.floor(Math.random() * 3)];
-const choice = prompt("Enter rock, paper or scissors: ");
-
-if (choice === randomChoice) {
-  console.log("Tie? Mabey try again next time");
-} else if (
-  (choice === "rock" && randomChoice === "paper") ||
-  (choice === "paper" && randomChoice === "scissors") ||
-  (choice === "scissors" && randomChoice === "rock")
-) {
-  console.log("You lost, Better luck next time");
-} else {
-  console.log("You won, Hip Hip Horray");
+/**
+ * The hourGlass() function.
+ *
+ * @param {number} userNum - number from user
+ * @param  {number} spaces - spaces from user
+ */
+function hourGlass(userNum: number, spaces: number = 0): void {
+  let string = ''
+  for (let counter: number = 0; counter < spaces; counter++) {
+    string += ' '
+  }
+  for (let counter1: number = 0; counter1 < userNum; counter1++) {
+    string += '* '
+  }
+  console.log(string + '\n')
+  if (userNum > 1) {
+    hourGlass(userNum - 1, spaces + 1)
+  }
+  console.log(string + '\n')
 }
-console.log("\nDone.");
+
+/**
+ * The main() function.
+ *
+ */
+function main(): void {
+  const prompt = promptSync()
+  const userString = prompt('Enter the hourglass length: ')
+  console.log('')
+  const userNumber = parseInt(userString)
+  if (isNaN(userNumber)) {
+    console.log('Not an number.')
+  } else if (userNumber <= 0) {
+    console.log('Invalid input')
+  }
+  hourGlass(userNumber)
+}
+main()
